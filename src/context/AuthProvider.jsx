d6913c301d3212bff9c73f2e0aa85a0ba742f13c
Harsh-sjce <harsh.sjce@gmail.com>
2024-09-01 17:40:37 +0530

    created

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+export const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [blogs, setBlogs] = useState();
+  const [profile, setProfile] = useState();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const token = Cookies.get("token");
+        const parsedToken = token ? JSON.parse(token) : undefined;
+        if (parsedToken) {
+          const { data } = await axios.get(
+            "http://localhost:4001/api/users/my-profile",
+            {
+              withCredentials: true,
+              headers: { "Content-Type": "application/json" },
+            }
+          );
+          console.log(data);
+          setProfile(data);
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchBlogs = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4001/api/blogs/all-blogs",
+          { withCredentials: true }
+        );
+        console.log(data);
+        setBlogs(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBlogs();
+    fetchProfile();
+  }, []);
+  return (
+    <AuthContext.Provider
+      value={{
+        blogs,
+        profile,
+        setProfile,
+        isAuthenticated,
+        setIsAuthenticated,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
